0b734eacd6b2b931c0827c1228c3f22c19903deb
MIKE Juan Victor <juanvictorqwerty@gmail.com>
2025-09-16 00:15:09 +0100

    cors configuration dev

--- backend/_Project/settings.py
+++ backend/_Project/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'ActionsOnFiles',
     'RegisterPublishers'
-
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173/'
 ]
 
 ROOT_URLCONF = '_Project.urls'
