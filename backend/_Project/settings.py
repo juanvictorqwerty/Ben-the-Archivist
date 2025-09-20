240a57c50f7d44d5c10c992df3515db4dd76733b
MIKE Juan Victor <juanvictorqwerty@gmail.com>
2025-09-20 14:13:18 +0100

    host allowed

--- backend/_Project/settings.py
+++ backend/_Project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bob-the-archivist.onrender.com/']
 
 
 # Application definition
